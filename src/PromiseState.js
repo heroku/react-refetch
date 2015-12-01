41bbf13f30cec2b6d6e12ecea16bcaad29ef80c1
Ryan Brainard <brainard@heroku.com>
2015-12-01 09:56:42 -0800

    Add and use methods to PromiseState for parity with Promise

--- src/PromiseState.js
+++ src/PromiseState.js
@@ -1,5 +1,39 @@
 export default class PromiseState {
 
+  // creates a new PromiseState that is pending
+  static create(meta) {
+    return new PromiseState({
+      pending: true,
+      meta: meta
+    })
+  }
+
+  // creates as PromiseState that is refreshing
+  // can be called without a previous PromiseState and will be both pending and refreshing
+  static refresh(previous, meta) {
+    const ps = previous || PromiseState.create(meta)
+    ps.refreshing = true
+    return ps
+  }
+
+  // creates a PromiseState that is resolved with the given value
+  static resolve(value, meta) {
+    return new PromiseState({
+      fulfilled: true,
+      value: value,
+      meta: meta
+    })
+  }
+
+  // creates a PromiseState that is rejected with the given reason
+  static reject(reason, meta) {
+    return new PromiseState({
+      rejected: true,
+      reason: reason,
+      meta: meta
+    })
+  }
+
   // The PromiseState.all(iterable) method returns a PromiseState
   // that resolves when all of the PromiseStates in the iterable
   // argument have resolved, or rejects with the reason of the
@@ -16,6 +50,25 @@ export default class PromiseState {
     })
   }
 
+  // The PromiseState.race(iterable) method returns a PromiseState
+  // that resolves or rejects as soon as one of the PromiseStates in
+  // the iterable resolves or rejects, with the value or reason
+  // from that PromiseState.
+  static race(iterable) {
+    const winner = iterable.find(ps => ps.settled)
+
+    return new PromiseState({
+      pending: !winner && iterable.some(ps => ps.pending),
+      refreshing: !winner && iterable.some(ps => ps.refreshing),
+      fulfilled: winner && winner.fulfilled,
+      rejected: winner && winner.rejected,
+      value: winner && winner.value,
+      reason: winner && winner.reason,
+      meta: winner && winner.meta
+    })
+  }
+
+  // Constructor for creating a raw PromiseState. DO NOT USE DIRECTLY. Instead, use PromiseState.create() or other static constructors
   constructor({ pending = false, refreshing = false, fulfilled = false, rejected = false, value = null, reason = null, meta = {} }) {
     this.pending = pending
     this.refreshing = refreshing
@@ -25,5 +78,39 @@ export default class PromiseState {
     this.value = value
     this.reason = reason
     this.meta = meta
+  }
+
+  // Appends and calls fulfillment and rejection handlers on the PromiseState,
+  // and returns a new PromiseState resolving to the return value of the called handler,
+  // or to its original settled value if the promise was not handled
+  // (i.e. if the relevant handler onFulfilled or onRejected is undefined).
+  // Note, unlike Promise.then(), these handlers are called immediately.
+  then(onFulFilled, onRejected) {
+    if (this.fulfilled && onFulFilled) {
+      return this._mapFlatMapValue(onFulFilled(this.value))
+    }
+
+    if (this.rejected && onRejected) {
+      return this._mapFlatMapValue(onRejected(this.reason))
+    }
+
+    return this
+  }
+
+  // Appends and calls a rejection handler callback to the PromiseState,
+  // and returns a new PromiseState resolving to the return value of the
+  // callback if it is called, or to its original fulfillment value if
+  // the PromiseState is instead fulfilled. Note, unlike Promise.catch(),
+  // this handlers is called immediately.
+  catch(onRejected) {
+    return this.then(undefined, onRejected)
+  }
+
+  _mapFlatMapValue(value) {
+    if (value instanceof PromiseState) {
+      return value
+    } else {
+      return PromiseState.resolve(value, this.meta)
+    }
   }
 }
