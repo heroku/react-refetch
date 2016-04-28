2d6e10bc3b4757eb739b685547956773b5e1572d
Daniel Schmidt <thelink64@hotmail.com>
2016-04-28 12:46:14 +0200

    Reject response with statusText

--- src/utils/handleResponse.js
+++ src/utils/handleResponse.js
@@ -8,6 +8,6 @@ export default function handleResponse(response) {
   if (response.status >= 200 && response.status < 300) { // TODO: support custom acceptable statuses
     return response.json() // TODO: support other response types
   } else {
-    return json.then(cause => Promise.reject(newError(cause)))
+    return Promise.reject(newError(response.statusText))
   }
 }
